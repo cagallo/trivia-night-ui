62c5b4296f9fa38d48fc3c6969b9fa3ae0d08852
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-03 10:41:29 -0700

    Add details to components

--- src/components/TriviaContainer.js
+++ src/components/TriviaContainer.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import CategoryCard from './CategoryCard'
 
 const TriviaContainer = () => {
   const categories = ['All Categories', 'Food and Drink', 'Geography', 'General knowledge', 'History', 'Art and Literature', 'Movies', 'Music', 'Science', 'Society and Culture', 'Sports and Leisure', 'User Generated Questions']
+  let i = 0;
   const triviaCategories = categories.map((category) => {
     return (
-       catergoryName = category
+      <CategoryCard
+       catergoryName ={category}
+       key ={i}
+       />
     )
   })
   return (
