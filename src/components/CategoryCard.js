610833dabecb4b8b1ac9301602bcf42c2b59aec9
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-03 18:09:48 -0700

    Add return to category card

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -1,5 +1,12 @@
 import React from "react";
+import '../styles/CategoryCard.css';
 
-const CategoryCard = () => {
-
+const CategoryCard = ({ categoryName }) => {
+  return (
+    <section className="category-card">
+      <h3>{categoryName}</h3>
+    </section>
+  )
 }
+
+export default CategoryCard;
