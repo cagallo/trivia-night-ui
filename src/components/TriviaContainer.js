265ed22b8082c7c1f0c065a1533a1e927674f406
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-03 18:11:34 -0700

    Add link to categories map function

--- src/components/TriviaContainer.js
+++ src/components/TriviaContainer.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import CategoryCard from './CategoryCard'
+import '../styles/TriviaContainer.css';
+import { Link } from 'react-router-dom';
 
 const TriviaContainer = () => {
   const categories = ['All Categories', 'Food and Drink', 'Geography', 'General knowledge', 'History', 'Art and Literature', 'Movies', 'Music', 'Science', 'Society and Culture', 'Sports and Leisure', 'User Generated Questions']
   let i = 0;
   const triviaCategories = categories.map((category) => {
-    return (
+    return <Link key={i++} to={`/${category}`}>
       <CategoryCard
-       catergoryName ={category}
-       key ={i}
-       />
-    )
+      categoryName ={category}
+      key ={i++}
+      />
+    </Link>
   })
+
   return (
     <section className="trivia-container">
+      <div className='trivia-container-text'>
+        <h3>Please choose a category or click "All Categories"</h3>
+      </div>
+      <div className='trivia-category-cards'>
       {triviaCategories}
+      </div>
     </section>
   )
 }
