4c926361e1275a76b3fe0145b8cc3b77a12ff9ac
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-03 18:08:52 -0700

    Change app to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import '../styles/App.css'
+import React from 'react';
+import '../styles/App.css';
+import TriviaContainer from './TriviaContainer';
+import Nav from './Nav';
+import TriviaGameView from './TriviaGameView';
+import { Route } from 'react-router-dom';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      triviaQuestions: []
-    }
-  }
-  render() {
-    return (
-      <h1>hey</h1>
-    )
-  }
+
+const App = () => {
+  return (
+    <section className='app'>
+      <Nav />
+      <Route path="/:category" render={({ match }) => {
+        console.log(match)
+        return <TriviaGameView category={match.params.category} />;
+      }} />
+      <Route exact path="/">
+        <TriviaContainer />
+      </Route>
+    </section>
+  )
 }
 
 export default App;
