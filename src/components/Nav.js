09aa22bd86cca8ef49efb3c5cfc22a86826ca371
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-03 18:11:02 -0700

    Comment out conditional

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
 const Nav = ( {location} ) => {
-    let navLink;
-    if (location === 'form') {
-      navLink = <Link className="landing-page-link" to="/"><h2>Return Home</h2></Link>
-    } else {
-      navLink = <Link className="form-link" to="/form"><h2>Add a New Question</h2></Link>
-    }
+    // let navLink;
+    // if (location === 'form') {
+    //   navLink = <Link className="landing-page-link" to="/"><h2>Return Home</h2></Link>
+    // } else {
+    //   navLink = <Link className="form-link" to="/form"><h2>Add a New Question</h2></Link>
+    // }
     return (
       <nav className="navbar">
         <h1>Trivia Night</h1>
-        {navLink}
+        {/* {navLink} */}
       </nav>
     )
   }
   
-}
-
 export default Nav;
