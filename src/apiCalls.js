a4f7154ad654d9402312379f579f821556d92579
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-05 09:52:05 -0700

    Remove console logs

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,11 @@
 export const apiCalls = {
 
   async getAllCategories() {
-    console.log('r we hittin dis')
     let response = await fetch('https://trivia-night-api-2110.herokuapp.com/api/v1/questions/all')
     return await response.json();
   },
 
   async getQuestionsByCategory(category) {
-    console.log(category)
     let response = await fetch(`https://trivia-night-api-2110.herokuapp.com/api/v1/questions?category=${category}`)
     return await response.json()
   },
