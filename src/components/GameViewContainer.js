765955e918a70e57252bce5c0719458032e95e0c
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-05 09:47:02 -0700

    Delete console logs

--- src/components/GameViewContainer.js
+++ src/components/GameViewContainer.js
@@ -4,25 +4,21 @@ import { shuffle } from '../utils';
 import '../styles/GameViewContainer.css';
 
 const GameViewContainer = (props) => {
-  console.log('rednering fucking everything')
+
   let index = props.index;
 
   if (props.questions.length < 1) {
     return "";
   }
-  console.log(index)
-  console.log(props.questions)
+
   const question = props.questions[index];
-  console.log(question)
   const incorrectAnswers = question.incorrect_answers;
   const correctAnswer = question.correct_answer;
 
   let shuffledAnswers;
   if (props.shuffledAnswers.length > 0) {
-    console.log("fuck")
     shuffledAnswers = props.shuffledAnswers;
   } else {
-    console.log("here")
     shuffledAnswers = shuffle([...incorrectAnswers, correctAnswer]);
   }
    
