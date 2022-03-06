75f3a95d80393699bd2ba1267813914735e9f8b4
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-05 20:32:23 -0700

    Create error component

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/ErrorPage.css';
+
+const ErrorPage = (props) => {
+  return (
+    <div className="error-page">
+      <h2 className="error">Sorry, there has been an error: </h2>
+      <p className="message">{props.message}</p>
+    </div>
+  );
+}
+
+export default ErrorPage;
