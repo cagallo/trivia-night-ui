188bf9ee079e0759e4aec383dd0849d7cabd6248
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-06 18:37:09 -0700

    Add proptypes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../styles/Nav.css'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import logo from '../cropped-trivia.png'
 
-const Nav = ( {location} ) => {
+const Nav = ( { location } ) => {
 	let navLink
 	if (location === 'home') {
 		navLink = <Link className="form-link" to="/form"><h2>Add a New Question</h2></Link>
 	}
-	const renderDirections = location === 'home' ? <h3 className='directions'>Please choose a category or click "All Categories"</h3> : null
+	const renderDirections = location === 'home' ? <h3 className='directions'>Please choose a category or click `All Categories`</h3> : null
     
 	return (
 		<nav className="navbar">
@@ -24,3 +25,7 @@ const Nav = ( {location} ) => {
 }
   
 export default Nav
+
+Nav.propTypes = {
+	location: PropTypes.string
+}
