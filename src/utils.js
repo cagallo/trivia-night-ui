b94dcc03e06d9bf253a8c14fc72a2c44806062ec
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-06 17:28:15 -0700

    Refactor handle response to render more specific details

--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,34 @@
-export const shuffle = (array) => {
-	let currentIndex = array.length, randomIndex
+export const shuffle = (incorrectAnswers, correctAnswer) => {
+	const maxAnswers = 4
+	const maxCorrect = 1
 
+	// limit number of answers shuffled to number we want to display
+	let possibleAnswers = incorrectAnswers.slice(0, maxAnswers - maxCorrect)
+	possibleAnswers.push(correctAnswer)
+
+	let currentIndex = possibleAnswers.length, randomIndex
 	while (currentIndex !== 0) {
 		randomIndex = Math.floor(Math.random() * currentIndex)
 		currentIndex--;
 
-		[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+		[possibleAnswers[currentIndex], possibleAnswers[randomIndex]] = [possibleAnswers[randomIndex], possibleAnswers[currentIndex]]
 	}
-
-	return array
-  
+	return possibleAnswers
 }
 
 
-export const handleResponse = (response) => {
+export const handleResponse = async(response) => {
+	let result = await response.json()
 	if (!response.ok) {
+		if (result) {
+			if (result.error.detail) {
+				throw new Error( `${result.error.detail}`)
+			}
+			throw new Error( `${result.error}`)
+		}
+		
 		throw new Error( `${response.status} ${response.statusText}: Unable to load content.`)
 	}
+
+	return result
 }
