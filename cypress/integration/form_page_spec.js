ba677abba063dfd3bdf04bedd5d74c3596303929
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-06 18:17:14 -0700

    Create form test

---
+++ cypress/integration/form_page_spec.js
@@ -0,0 +1,32 @@
+describe('Trivia Night form page user flow', () => {
+  
+
+	it('should update input values as form is filled out', () => {
+		cy.visit('http://localhost:3000/form')
+			.get('input[name="question"]').type('What year did the French Revolution begin?').should('have.value', 'What year did the French Revolution begin?')
+			.get('input[name="incorrect_answer_1"]').type('1820').should('have.value', '1820')
+			.get('input[name="incorrect_answer_2"]').type('1861').should('have.value', '1861')
+			.get('input[name="incorrect_answer_3"]').type('1799').should('have.value', '1799')
+			.get('input[name="correct_answer"]').type('1789').should('have.value', '1789')
+	})
+  
+	it('should successfully post question based on user input', () => {
+		cy.intercept('POST', 'https://trivia-night-api-2110.herokuapp.com/api/v1/questions',  
+			{
+				statusCode: 201,
+				body: {
+					'question': 'What year did the French Revolution begin?',
+					'incorrect_answers': ['1820', '1861', '1799'],
+					'correct_answer': 1789,
+				}
+			})
+		cy.visit('http://localhost:3000/form')
+			.get('input[name="question"]').type('What year did the French Revolution begin?').should('have.value', 'What year did the French Revolution begin?')
+			.get('input[name="incorrect_answer_1"]').type('1820').should('have.value', '1820')
+			.get('input[name="incorrect_answer_2"]').type('1861').should('have.value', '1861')
+			.get('input[name="incorrect_answer_3"]').type('1799').should('have.value', '1799')
+			.get('input[name="correct_answer"]').type('1789').should('have.value', '1789')
+			.get('.new-question-button').click()
+			.get('.message').contains('Your question: "What year did the French Revolution begin?" was created successfully!')
+	})
+})
