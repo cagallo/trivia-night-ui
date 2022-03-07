9712790ddc70159597bb4e916cb1eb5aa32f7a35
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-06 17:31:57 -0700

    Change name of prop types to clean up parameter field

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,56 +1,64 @@
 import React from 'react'
+import { shuffle } from '../utils'
 import '../styles/QuestionCard.css'
 
 const QuestionCard = (props) => {
-	const possibleAnswers = [...props.shuffledAnswers, props.correctAnswer]
-	console.log(possibleAnswers)
+	const currentQuestion = props.currentQuestion
+	const correctAnswer = currentQuestion.correct_answer;
+
+	let possibleAnswers = shuffle(currentQuestion.incorrect_answers, correctAnswer)
+	if (props.shuffledAnswers.length > 0) {
+		possibleAnswers = props.shuffledAnswers
+	}
+
+	let option1,option2,option3,option4
+	[option1, option2, option3, option4] = [...possibleAnswers]
+
 	return (
-		<section className="single-question-card" id={props.id}>
-			<span className="question-total">{`Question ${props.index + 1} of ${props.total}`}</span>
-			<h3 className="current-question">{props.question}</h3>
+		<section className="single-question-card" id={currentQuestion.id}>
+			<span className="question-total">{`Question ${props.index + 1} of ${props.totalQuestions}`}</span>
+			<h3 className="current-question">{currentQuestion.question}</h3>
 			<div className="possible-answers-container">
 				<div className="top-row-answers">
 					<div className="top-left">
 						<div className="radio-button">
 							<input type="radio" id="possibleAnswer1" name="answer-button" value="possibleAnswer1" onClick={() => {
-								console.log('clicked')
-								props.checkAnswer(possibleAnswers[0], props.shuffledAnswers)
+								props.setAnswer(option1, possibleAnswers)
 							}}></input>
-							<label className="possible-answer">{possibleAnswers[0]}</label>
+							<label className="possible-answer">{option1}</label>
 						</div>
 					</div>
 					<div className="top-right">
 						<div className="radio-button">
 							<input type="radio" id="possibleAnswer2" name="answer-button" value="possibleAnswer2" onClick={() => {
-								props.checkAnswer(possibleAnswers[1], props.shuffledAnswers)
+								props.setAnswer(option2, possibleAnswers)
 							}}></input>
 						</div>
-						<label className="possible-answer">{possibleAnswers[1]}</label>
+						<label className="possible-answer">{option2}</label>
 					</div>
 				</div>
 				<div className="bottom-row-answers">
 					<div className="bottom-left">
 						<div className="radio-button">
 							<input type="radio" id="possibleAnswer3" name="answer-button" value="possibleAnswer3" onClick={() => {
-								props.checkAnswer(possibleAnswers[2], props.shuffledAnswers)
+								props.setAnswer(option3, possibleAnswers)
 							}}></input>
-							<label className="possible-answer">{possibleAnswers[2]}</label>
+							<label className="possible-answer">{option3}</label>
 						</div>
 					</div>
 					<div className="bottom-right">
 						<div className="radio-button">
 							<input type="radio" id="possibleAnswer4" name="answer-button" value="possibleAnswer4" onClick={() => {
-								props.checkAnswer(possibleAnswers[3], props.shuffledAnswers)
+								props.setAnswer(option4, possibleAnswers)
 							}}></input>
-							<label className="possible-answer">{possibleAnswers[3]}</label>
+							<label className="possible-answer">{option4}</label>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div className="answer-button-container">
-				<button disabled={props.disabled} className="submit-answer-button" onClick={() => {
-					console.log('clicked')
-					props.handleNextQuestion(props.correctAnswer)
+				<button disabled={props.submitBtnDisabled} className="submit-answer-button" onClick={() => {
+					props.handleNextQuestion(correctAnswer)
 				}}>Submit</button>
 			</div>
 		</section>
