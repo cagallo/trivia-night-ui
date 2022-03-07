25447c75690dbb3aa8dc1f6b57a3975a69231a3e
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-06 17:29:42 -0700

    Add more detailed error messages

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -2,10 +2,15 @@ import React from 'react'
 import '../styles/ErrorPage.css'
 
 const ErrorPage = (props) => {
+	let error
+	if (props.message) {
+		error = props.message.toString()
+		error = error.substring(error.indexOf('Error: ') + 7)
+	}
 	return (
 		<div className="error-page">
 			<h2 className="error">Sorry, there has been an error: </h2>
-			<p className="message">{props.message}</p>
+			<p className="message">{error}</p>
 		</div>
 	)
 }
