fb6c5b54e3af7758990cf6fad5e7d05a995ee5fc
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-06 18:17:37 -0700

    Add error handling for post in form

--- cypress/integration/error_spec.js
+++ cypress/integration/error_spec.js
@@ -18,4 +18,41 @@ describe('Trivia Night error page user flow', () => {
 			.get('.message').contains('500 Internal Server Error: Unable to load content')
 	})
 
+	it('should display error when user tries to submit duplicate question', () => {
+		cy.intercept('POST', 'https://trivia-night-api-2110.herokuapp.com/api/v1/questions',  
+			{
+				statusCode: 201,
+				body: {
+					'question': 'What year did the French Revolution begin?',
+					'incorrect_answers': ['1820', '1861', '1799'],
+					'correct_answer': 1789,
+				}
+			})
+		cy.visit('http://localhost:3000/form')
+			.get('input[name="question"]').type('What year did the French Revolution begin?').should('have.value', 'What year did the French Revolution begin?')
+			.get('input[name="incorrect_answer_1"]').type('1820').should('have.value', '1820')
+			.get('input[name="incorrect_answer_2"]').type('1861').should('have.value', '1861')
+			.get('input[name="incorrect_answer_3"]').type('1799').should('have.value', '1799')
+			.get('input[name="correct_answer"]').type('1789').should('have.value', '1789')
+			.get('.new-question-button').click()
+			.get('.message').contains('Your question: "What year did the French Revolution begin?" was created successfully!')
+
+		cy.intercept('POST', 'https://trivia-night-api-2110.herokuapp.com/api/v1/questions',  
+			{
+				statusCode: 201,
+				body: {
+					'question': 'What year did the French Revolution begin?',
+					'incorrect_answers': ['1820', '1861', '1799'],
+					'correct_answer': 1789,
+				}
+			})
+		cy.visit('http://localhost:3000/form')
+			.get('input[name="question"]').type('What year did the French Revolution begin?').should('have.value', 'What year did the French Revolution begin?')
+			.get('input[name="incorrect_answer_1"]').type('1820').should('have.value', '1820')
+			.get('input[name="incorrect_answer_2"]').type('1861').should('have.value', '1861')
+			.get('input[name="incorrect_answer_3"]').type('1799').should('have.value', '1799')
+			.get('input[name="correct_answer"]').type('1789').should('have.value', '1789')
+			.get('.new-question-button').click()
+			.get('.message').contains('Your question: "What year did the French Revolution begin?" was created successfully!')
+	})
 })
