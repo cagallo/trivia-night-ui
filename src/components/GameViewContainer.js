3a89cb0e414bf897beaa4181e060592b310f2c9d
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-06 17:29:20 -0700

    Clean up props

--- src/components/GameViewContainer.js
+++ src/components/GameViewContainer.js
@@ -1,49 +1,23 @@
 import React from 'react'
 import QuestionCard from './QuestionCard'
-import { shuffle } from '../utils'
+
 import '../styles/GameViewContainer.css'
 
 const GameViewContainer = (props) => {
-
-	let index = props.index
-
-	if (props.questions.length < 1) {
-		return ''
-	}
-	console.log(props.questions)
-	const question = props.questions[index]
-	console.log(question)
-	const incorrectAnswers = question.incorrect_answers
-	const correctAnswer = question.correct_answer
-
-	let shuffledAnswers
-	if (props.shuffledAnswers.length > 0) {
-		shuffledAnswers = props.shuffledAnswers
-	} else {
-		shuffledAnswers = shuffle([...incorrectAnswers, correctAnswer])
-	}
-   
-	let questionCard = (
-		<QuestionCard
-			key={index}
-			index={index}
-			disabled={props.disabled}
-			score={props.score}
-			allQuestions={props.questions}
-			question={question.question}
-			shuffledAnswers={shuffledAnswers}
-			correctAnswer={correctAnswer}
-			id={question.id}
-			checkAnswer={props.checkAnswer}
-			finishGame={props.finishGame}
-			handleNextQuestion={props.handleNextQuestion}
-			total={props.total}
-		/>
-	)
-  
+	const index = props.index
+	const question = props.currentQuestion  
 	return (
 		<section className='game-view-container'>
-			{questionCard}
+			<QuestionCard
+				key={index}
+				index={index}
+				totalQuestions={props.totalQuestions}
+				currentQuestion={question}
+				shuffledAnswers={props.shuffledAnswers}
+				submitBtnDisabled={props.submitBtnDisabled}
+				setAnswer={props.setAnswer}
+				handleNextQuestion={props.handleNextQuestion}
+			/>
 		</section>
 	)
 
