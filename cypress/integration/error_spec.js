1bb3fc49de6934acce6bd6c4cb401e12d5d03a01
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-03-06 19:21:30 -0700

    Finish error testing

--- cypress/integration/error_spec.js
+++ cypress/integration/error_spec.js
@@ -4,7 +4,7 @@ describe('Trivia Night error page user flow', () => {
 	it('should display 404 page when visiting a bad link', () => {
 		cy.visit('http://localhost:3000/4823483290434903')
 			.get('.error').contains('Sorry, there has been an error: ')
-			.get('.message').contains('404 Not Found: Unable to load content')
+			.get('.message').contains('404 Not Found: Unable to load content.')
 			.get('.logo-link').click()
 		cy.location().should((loc) => {
 			expect(loc.href).to.eq('http://localhost:3000/')
@@ -39,7 +39,7 @@ describe('Trivia Night error page user flow', () => {
 
 		cy.intercept('POST', 'https://trivia-night-api-2110.herokuapp.com/api/v1/questions',  
 			{
-				statusCode: 201,
+				statusCode: 500,
 				body: {
 					'question': 'What year did the French Revolution begin?',
 					'incorrect_answers': ['1820', '1861', '1799'],
@@ -53,6 +53,28 @@ describe('Trivia Night error page user flow', () => {
 			.get('input[name="incorrect_answer_3"]').type('1799').should('have.value', '1799')
 			.get('input[name="correct_answer"]').type('1789').should('have.value', '1789')
 			.get('.new-question-button').click()
-			.get('.message').contains('Your question: "What year did the French Revolution begin?" was created successfully!')
+			.get('.error').contains('Sorry, there has been an error: ')
+			.get('.message').contains('500 Internal Server Error: Unable to load content')
+	})
+
+	it('should display an error if an incomplete form is submitted', () => {
+    
+		cy.intercept('POST', 'https://trivia-night-api-2110.herokuapp.com/api/v1/questions',  
+			{
+				statusCode: 422,
+				body: {
+					'question': 'Which founding father is responsible for founding the New York Post?',
+					'incorrect_answers': ['Benjamin Franklin', 'John Adams', 'Thomas Jefferson'],
+					'correct_answer': '',
+				}
+			})
+		cy.visit('http://localhost:3000/form')
+			.get('input[name="question"]').type('Which founding father is responsible for founding the New York Post?').should('have.value', 'Which founding father is responsible for founding the New York Post?')
+			.get('input[name="incorrect_answer_1"]').type('Benjamin Franklin').should('have.value', 'Benjamin Franklin')
+			.get('input[name="incorrect_answer_2"]').type('John Adams').should('have.value', 'John Adams')
+			.get('input[name="incorrect_answer_3"]').type('Thomas Jefferson').should('have.value', 'Thomas Jefferson')
+			.get('.new-question-button').click()
+			.get('.error').contains('Sorry, there has been an error: ')
+			.get('.message').contains('422 Unprocessable Entity: Unable to load content')
 	})
 })
